f9778e09d3235aa655b686f3ec5fa247b8201d7d
VirendraRaut <virendraraut1306@gmail.com>
2024-10-14 16:26:50 +0530

    giving css  with classname by using styles obj of style.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import styles from "./style";
+
 export default function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}></div>
+      </div>
     </div>
-  )
+  );
 }
