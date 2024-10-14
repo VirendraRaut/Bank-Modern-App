d01376cccb7421e0a31e4b128e25c7c66bfedcd2
VirendraRaut <virendraraut1306@gmail.com>
2024-10-14 16:22:20 +0530

    div element in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <h1 className="bg-primary w-full overflow-hidden">
-      Hello world!
-    </h1>
+    <div className="bg-primary w-full overflow-hidden">
+      
+    </div>
   )
 }
