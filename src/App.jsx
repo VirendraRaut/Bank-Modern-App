befd2ff3dcaf8f2fd54cc697d489418e5c636e5e
VirendraRaut <virendraraut1306@gmail.com>
2024-10-14 16:15:25 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="bg-primary w-full overflow-hidden text-white p-20">
+    <h1 className="bg-primary w-full overflow-hidden">
       Hello world!
     </h1>
   )
