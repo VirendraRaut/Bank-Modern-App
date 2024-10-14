2918525e0c0b95b70a2fbb2a06b946cccab9eeda
VirendraRaut <virendraraut1306@gmail.com>
2024-10-14 16:40:03 +0530

    testing ui of page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ export default function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}></div>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
       </div>
     </div>
   );
