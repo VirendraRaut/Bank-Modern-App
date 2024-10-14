b14cad144da2a74866a3522722a531085d68fecd
VirendraRaut <virendraraut1306@gmail.com>
2024-10-14 16:15:07 +0530

    testing tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="bg-primary w-full overflow-hidden">
+    <h1 className="bg-primary w-full overflow-hidden text-white p-20">
       Hello world!
     </h1>
   )
