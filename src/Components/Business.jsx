5d8a197e206e943defe4ce69440e371cd5a069b1
VirendraRaut <virendraraut1306@gmail.com>
2024-10-16 21:52:36 +0530

    dynamic css

--- src/Components/Business.jsx
+++ src/Components/Business.jsx
@@ -6,7 +6,7 @@ const Business = () => {
   return (
     <section id="features" className={`${layout.section} `}>
       <div className={`${layout.sectionInfo}`}>
-        <h2>
+        <h2 className={`${styles.heading2}`}>
           You do the business, <br className="sm:block hidden" />
           We'll handle the money.
         </h2>
