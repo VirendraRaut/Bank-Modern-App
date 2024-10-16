b366edf723656879a090086209463ab177074d4f
VirendraRaut <virendraraut1306@gmail.com>
2024-10-16 22:07:57 +0530

    created button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <button
+      type="button"
+      className={`px-6 py-4 bg-blue-gradient outline-none text-[18px]`}
+    >
+      Get Started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
