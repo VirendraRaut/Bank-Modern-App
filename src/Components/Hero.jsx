3f1c2b58e04c193ada8ab16660e950c8d62904b9
VirendraRaut <virendraraut1306@gmail.com>
2024-10-16 19:12:50 +0530

    GetStarted component imported for mobile device view

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -46,6 +46,10 @@ const Hero = () => (
       <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient"></div>
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"></div>
     </div>
+
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <GetStarted />
+    </div>
   </section>
 );
 
