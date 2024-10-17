7c9bd9f634d5b5f32616f5cb5d32b79e70bedb28
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 17:01:16 +0530

    heading tag

--- src/Components/CardDeal.jsx
+++ src/Components/CardDeal.jsx
@@ -3,7 +3,17 @@ import Button from "./Button";
 import { card } from "../assets";
 
 const CardDeal = () => {
-  return <div></div>;
+  return (
+    <section className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          Find a better card deal,
+          <br className="sm:block hidden" />
+          In few easy steps
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
