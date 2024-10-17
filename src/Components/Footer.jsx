3f134ead5dc25991ef8962c0d83f32e172526747
VirendraRaut <virendraraut1306@gmail.com>
2024-10-18 02:22:03 +0530

    Project is completed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -48,7 +48,16 @@ const Footer = () => (
       </p>
 
       <div className="flex flow-row md:mt-0 mt-6">
-        
+        {socialMedia.map((social, index) => (
+          <img
+            key={social.id}
+            src={social.icon}
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+              index !== socialMedia.links.length - 1 ? "mr-6" : "mr-0"
+            }`}
+          />
+        ))}
       </div>
     </div>
   </section>
