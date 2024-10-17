a91466fd90c26b8747c359427d78d1196ce96b8b
VirendraRaut <virendraraut1306@gmail.com>
2024-10-18 02:02:23 +0530

    error fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -19,10 +19,10 @@ const Footer = () => (
       <div className="flex flex-[1.5] flex-row flex-wrap w-full justify-between md:mt-0 mt-10">
         {footerLinks.map((footerLink) => (
           <div
-            key={footerLink.key}
+            key={footerLink.title}
             className="flex flex-col sm:my-0 my-4 min-w-[150px]"
           >
-            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-kWhite">
+            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
               {footerLink.title}
             </h4>
             <ul>
