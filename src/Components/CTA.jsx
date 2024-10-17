2f39a14b3c00f448061e3358b9a572a5c081f821
VirendraRaut <virendraraut1306@gmail.com>
2024-10-18 01:07:25 +0530

    created heading and its summary i.e. p tag

--- src/Components/CTA.jsx
+++ src/Components/CTA.jsx
@@ -6,8 +6,11 @@ const CTA = () => (
     className={`${styles.flexCenter} ${styles.padding} ${styles.marginY} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
   >
     <div className={`flex flex-1 flex-col`}>
-      <h2></h2>
-      <p></p>
+      <h2 className={`${styles.heading2}`}>Let's try our service now!</h2>
+      <p className={`${styles.paragraph}`}>
+        Everything you need to accept card payments and grow your business
+        anywhere on the planet
+      </p>
     </div>
   </section>
 );
