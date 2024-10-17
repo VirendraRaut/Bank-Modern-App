c28158e39d176062870f014fa6dbc81fe4bf3223
VirendraRaut <virendraraut1306@gmail.com>
2024-10-18 01:26:05 +0530

    imported image

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,13 @@ import { socialMedia, footerLinks } from "../constants";
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-      <div className="flex flex-1 flex-col justify-start mr-10"></div>
+      <div className="flex flex-1 flex-col justify-start mr-10">
+        <img
+          src={logo}
+          alt="hoobank"
+          className="w-[266px] h-[72px] object-contain"
+        />
+      </div>
     </div>
   </section>
 );
