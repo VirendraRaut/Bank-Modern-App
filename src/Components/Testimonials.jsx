cf4c3751fea990b67f1138a73eba7fb141c562a6
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 17:33:00 +0530

    css to heading tag

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -10,7 +10,7 @@ const Testimonials = () => {
       <div />
 
       <div className="flex w-full md:flex-row flex-col justify-between items-center sm:mb-16 mb-6">
-        <h1>
+        <h1 className={`${styles.heading2}`}>
           What people are <br className="sm:block hidden" />
           saying about us
         </h1>
