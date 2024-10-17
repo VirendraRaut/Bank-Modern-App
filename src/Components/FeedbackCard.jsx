e72219cb20d1f3f0b06f1d30ecd5e3d07ab94ae4
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 18:33:50 +0530

    try to solve error

--- src/Components/FeedbackCard.jsx
+++ src/Components/FeedbackCard.jsx
@@ -1,7 +1,7 @@
 import { quotes } from "../assets";
 
 const FeedbackCard = ({ content, name, title, img }) => (
-  <div className="flex flex-col justify-between px-10 py-12 rounded-[20px] max-w-[370px] feedback-card md:mr-10 sm:mr-5 mr-0 my-5">
+  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] feedback-card md:mr-10 sm:mr-5 mr-0 my-5">
     <img
       src={quotes}
       alt="double_quotes"
