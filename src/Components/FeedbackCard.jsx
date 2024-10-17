28ca447f90c8f133c4aa761d89d4844d9fd58368
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 18:50:27 +0530

    undo

--- src/Components/FeedbackCard.jsx
+++ src/Components/FeedbackCard.jsx
@@ -1,8 +1,15 @@
-import quotes from "../assets/quotes.svg";
+import { quotes } from "../assets";
 
-const FeedbackCard = ({ content, name, title, icon }) => (
+const FeedbackCard = ({ content, name, title, img }) => (
   <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] feedback-card md:mr-10 sm:mr-5 mr-0 my-5">
-    <img src={quotes} alt="" />
+    <img
+      src={quotes}
+      alt="double_quotes"
+      className="w-[42px] h-[27px] object-contain"
+    />
+    <p className="font-poppins font-normal text-[18px] leading-[32px] text-white my-10">
+      {content}
+    </p>
   </div>
 );
 
