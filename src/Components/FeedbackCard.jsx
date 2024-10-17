ea195956a0992f5032c888567b5d8e33176be46b
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 18:27:46 +0530

    error not solved

--- src/Components/FeedbackCard.jsx
+++ src/Components/FeedbackCard.jsx
@@ -1,16 +1,14 @@
 import { quotes } from "../assets";
 
-const FeedbackCard = ({ content, name, title,img }) => {
-  return (
-    <div className="flex flex-col justify-between px-10 py-12 rounded-[20px] max-w-[370px] feedback-card md:mr-10 sm:mr-5 mr-0 my-5">
-      <img
-        src={quotes}
-        alt="double_quotes"
-        className="w-[42px] h-[27px] object-contain"
-      />
-      <p className="font-poppins font-normal text-white">{content}</p>
-    </div>
-  );
-};
+const FeedbackCard = ({ content, name, title, img }) => (
+  <div className="flex flex-col justify-between px-10 py-12 rounded-[20px] max-w-[370px] feedback-card md:mr-10 sm:mr-5 mr-0 my-5">
+    <img
+      src={quotes}
+      alt="double_quotes"
+      className="w-[42px] h-[27px] object-contain"
+    />
+    <p className="font-poppins font-normal text-white">{content}</p>
+  </div>
+);
 
 export default FeedbackCard;
