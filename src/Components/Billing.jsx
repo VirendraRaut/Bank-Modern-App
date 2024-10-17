d448af59e714c6d6c33c82b65fc246910f61be96
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 15:47:39 +0530

    immported biiling image

--- src/Components/Billing.jsx
+++ src/Components/Billing.jsx
@@ -1,10 +1,17 @@
 import styles, { layout } from "../style";
 import { apple, bll, google } from "../constants";
+import { bill } from "../assets";
 
 const Billing = () => {
   return (
     <section id="product" className={`${layout.sectionReverse}`}>
-      <div></div>
+      <div className={`${layout.sectionImgReverse}`}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+      </div>
     </section>
   );
 };
