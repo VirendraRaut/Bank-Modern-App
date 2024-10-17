90435b8f3e6a1b8cda8bd1e42279a662a3b7202f
VirendraRaut <virendraraut1306@gmail.com>
2024-10-18 00:22:37 +0530

    completing feedbackcard

--- src/Components/FeedbackCard.jsx
+++ src/Components/FeedbackCard.jsx
@@ -17,6 +17,13 @@ const FeedbackCard = ({ content, name, title, img }) => (
         alt="user_profile"
         className="w-[48px] h-[48px] rounded-full"
       />
+
+      <div className="flex flex-col ml-4">
+        <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">
+          {name}
+        </h4>
+        <p>{title}</p>
+      </div>
     </div>
   </div>
 );
