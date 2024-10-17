7dcdf6ad21c8a570d23f2e9c4f7d3695e518f07b
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 16:18:56 +0530

    formatting

--- src/Components/Billing.jsx
+++ src/Components/Billing.jsx
@@ -8,8 +8,10 @@ const Billing = () => {
         <img
           src={bill}
           alt="billing"
-          className="w-[100%] h-[100%] relative z-[5]"
+          className="w-[80%] h-[80%] relative z-[5]"
         />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
       </div>
     </section>
   );
