42528dcb23360f8f2dfaf1d41e36ea75742c1f7d
VirendraRaut <virendraraut1306@gmail.com>
2024-10-18 01:04:39 +0530

    css

--- src/Components/CTA.jsx
+++ src/Components/CTA.jsx
@@ -1,6 +1,15 @@
 import styles from "../style";
 import Button from "./Button";
 
-const CTA = () => <section></section>;
+const CTA = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.padding} ${styles.marginY} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
+    <div className={`flex flex-1 flex-col`}>
+      <h2></h2>
+      <p></p>
+    </div>
+  </section>
+);
 
 export default CTA;
