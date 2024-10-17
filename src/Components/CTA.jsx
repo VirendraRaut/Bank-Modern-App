ecb4269878d328a26eebc75a47954cc0e791c682
VirendraRaut <virendraraut1306@gmail.com>
2024-10-18 01:08:15 +0530

    css for p tag

--- src/Components/CTA.jsx
+++ src/Components/CTA.jsx
@@ -7,7 +7,7 @@ const CTA = () => (
   >
     <div className={`flex flex-1 flex-col`}>
       <h2 className={`${styles.heading2}`}>Let's try our service now!</h2>
-      <p className={`${styles.paragraph}`}>
+      <p className={`${styles.paragraph} max-w-[470px]`}>
         Everything you need to accept card payments and grow your business
         anywhere on the planet
       </p>
