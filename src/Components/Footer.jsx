80ccda04dbfdff7ea45143c4508be4e112cee192
VirendraRaut <virendraraut1306@gmail.com>
2024-10-18 01:24:40 +0530

    start created component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,12 @@ import styles from "../style";
 import { logo } from "../assets";
 import { socialMedia, footerLinks } from "../constants";
 
-const Footer = () => <section></section>;
+const Footer = () => (
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+      <div className="flex flex-1 flex-col justify-start mr-10"></div>
+    </div>
+  </section>
+);
 
 export default Footer;
