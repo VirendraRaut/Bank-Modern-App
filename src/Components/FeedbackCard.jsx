e028393d9100662650afe1699b9ad1405258aa8e
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 18:23:08 +0530

    image is created

--- src/Components/FeedbackCard.jsx
+++ src/Components/FeedbackCard.jsx
@@ -2,7 +2,13 @@ import { quotes } from "../assets";
 
 const FeedbackCard = ({ content, name, title }) => {
   return (
-    <div className="flex flex-col justify-between px-10 py-12 rounded-[20px] max-w-[370px] feedback-card md:mr-10 sm:mr-5 mr-0 my-5"></div>
+    <div className="flex flex-col justify-between px-10 py-12 rounded-[20px] max-w-[370px] feedback-card md:mr-10 sm:mr-5 mr-0 my-5">
+      <img
+        src={quotes}
+        alt="double_quotes"
+        className="w-[42px] h-[27px] object-contain"
+      />
+    </div>
   );
 };
 
