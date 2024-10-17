79d63ce432d94c75fe8d1fbd2dbbb61e17646c0b
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 16:43:55 +0530

    google play logo

--- src/Components/Billing.jsx
+++ src/Components/Billing.jsx
@@ -29,8 +29,13 @@ const Billing = () => {
         <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
           <img
             src={apple}
-            alt="google_play"
+            alt="apple_store"
             className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="google_play"
+            className="w-[128px] h-[42px] object-contain cursor-pointer"
           />
         </div>
       </div>
