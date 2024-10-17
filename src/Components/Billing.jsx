c29a680827a71b300c9be771ec832f9cd2cad6f9
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 16:22:46 +0530

    heading tag in Billing

--- src/Components/Billing.jsx
+++ src/Components/Billing.jsx
@@ -13,6 +13,13 @@ const Billing = () => {
         <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
         <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
       </div>
+
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          Easily control your <br className="sm:block hidden" />
+          Billing & Invoicing
+        </h2>
+      </div>
     </section>
   );
 };
