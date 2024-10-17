30a464d277436b13e92f5c14b897b74e7c1ea651
VirendraRaut <virendraraut1306@gmail.com>
2024-10-17 18:26:10 +0530

    error

--- src/Components/FeedbackCard.jsx
+++ src/Components/FeedbackCard.jsx
@@ -1,6 +1,6 @@
 import { quotes } from "../assets";
 
-const FeedbackCard = ({ content, name, title }) => {
+const FeedbackCard = ({ content, name, title,img }) => {
   return (
     <div className="flex flex-col justify-between px-10 py-12 rounded-[20px] max-w-[370px] feedback-card md:mr-10 sm:mr-5 mr-0 my-5">
       <img
