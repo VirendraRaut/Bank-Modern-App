45f717f2a5aea0726601988b78ddbeb038d65acc
VirendraRaut <virendraraut1306@gmail.com>
2024-10-18 00:51:41 +0530

    created clinet component successfully

--- src/Components/Client.jsx
+++ src/Components/Client.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import styles from "../style";
+import { clients } from "../constants";
 
-const Client = () => {
-  return (
-    <div>
-      
+const Client = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 ${flexCenter} sm:min-w-[192px] min-w-[120]`}
+        >
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] object-contain"
+          />
+        </div>
+      ))}
     </div>
-  )
-}
-
-export default Client
+  </section>
+);
+export default Client;
